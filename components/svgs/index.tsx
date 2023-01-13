522137161cc445b29bbd2f65542c3c3b8cb3cb5a
Carol Enache <enache.carol@gmail.com>
2023-01-13 23:25:07 +0000

    degug build

--- components/svgs/index.tsx
+++ components/svgs/index.tsx
@@ -1,6 +1,5 @@
 // Todo export one Icon component from this file that takes a name prop and renders the correct icon
 
-export { default as MathematicalSymbol } from "./MathematicalSymbol";
 export { default as IntegralSymbol } from "./Integral";
 export { default as Estimate } from "./Estimate";
 export { default as Analysis } from "./Analysis";
