364761f1075fc1f97563bf86d3ff121544954052
CarolEnache1 <carol.enache@bt.com>
2023-11-08 23:42:33 +0000

    Export the required data from OECD for the Footprint

--- pages/api/sandbox.ts
+++ pages/api/sandbox.ts
@@ -2,13 +2,15 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 // import { oecdCoeficients } from '../../server/utils/oecdCoeficients';
 // import { unido } from '../../server/utils/unido';
-import { msr } from '../../server/utils/msr'
+// import { msr } from '../../server/utils/msr'
+import { getOECDData } from '../../server/utils/footprint';
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  res.status(200).json(msr());
+  // res.status(200).json(msr());
   // res.status(200).json(unido());
   // res.status(200).json(oecdCoeficients());
+  res.status(200).json(getOECDData());
 }
